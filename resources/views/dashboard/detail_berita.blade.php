7adc6f0fa78994381ec08f2c8cc2191d4308fcf0
danikaharu <dani.kaharu17@gmail.com>
2023-06-25 22:06:05 +0800

    change variable name

--- resources/views/dashboard/detail_berita.blade.php
+++ resources/views/dashboard/detail_berita.blade.php
@@ -20,18 +20,18 @@
                                   <div class="col-lg-12">
                                       <!-- News Detail Start -->
                                       <div class="position-relative mb-3">
-                                          <img class="img-fluid w-100" src="{{ $article['thumbnail'] }}"
+                                          <img class="img-fluid w-100" src="{{ $detailArticles['thumbnail'] }}"
                                               style="object-fit: cover;">
                                           <div class="overlay position-relative bg-light">
                                               <div class="mb-3">
-                                                  <p>by {{ $article['author'] }}</p>
-                                                  <a href="">{{ $article['category'] }}</a>
+                                                  <p>by {{ $detailArticles['author'] }}</p>
+                                                  <a href="">{{ $detailArticles['category'] }}</a>
                                                   <span class="px-1">/</span>
-                                                  <span>{{ $article['published_at'] }}</span>
+                                                  <span>{{ $detailArticles['published_at'] }}</span>
                                               </div>
                                               <div>
-                                                  <h3 class="mb-3">{{ $article['title'] }}</h3>
-                                                  {!! $article['body'] !!}
+                                                  <h3 class="mb-3">{{ $detailArticles['title'] }}</h3>
+                                                  {!! $detailArticles['body'] !!}
                                               </div>
                                           </div>
                                       </div>
@@ -48,88 +48,26 @@
                               <!-- Popular News Start -->
                               <div class="pb-3">
                                   <div class="bg-light py-2 px-4 mb-3">
-                                      <h3 class="m-0">Tranding</h3>
+                                      <h3 class="m-0">Trending</h3>
                                   </div>
-                                  <div class="d-flex mb-3">
-                                      <a href="{{ route('detailBerita', $articles['data'][0]['slug']) }}"><img
-                                              src="{{ $articles['data'][0]['thumbnail'] }}" alt=""
-                                              style="width: 100px; height:
-                                      100px; object-fit: cover;"></a>
-                                      <div class="w-100 d-flex flex-column justify-content-center bg-light px-3"
-                                          style="height: 100px;">
-                                          <div class="mb-1" style="font-size: 13px;">
-                                              <a href="">{{ $articles['data'][0]['category'] }}</a>
-                                              <span class="px-1">/</span>
-                                              <span>{{ $articles['data'][0]['published_at'] }}</span>
+                                  @foreach (array_slice($articles['data'], 0, 5) as $article)
+                                      <div class="d-flex mb-3">
+                                          <a href="{{ route('detailBerita', $article['slug']) }}"><img
+                                                  src="{{ $article['thumbnail'] }}" alt=""
+                                                  style="width: 100px; height:
+                                    100px; object-fit: cover;"></a>
+                                          <div class="w-100 d-flex flex-column justify-content-center bg-light px-3"
+                                              style="height: 100px;">
+                                              <div class="mb-1" style="font-size: 13px;">
+                                                  <a href="">{{ $article['category'] }}</a>
+                                                  <span class="px-1">/</span>
+                                                  <span>{{ $article['published_at'] }}</span>
+                                              </div>
+                                              <a class="h6 m-0"
+                                                  href="">{{ Str::limit($article['title'], 55, '....') }}</a>
                                           </div>
-                                          <a class="h6 m-0"
-                                              href="">{{ Str::limit($articles['data'][0]['title'], 55, '....') }}</a>
                                       </div>
-                                  </div>
-                                  <div class="d-flex mb-3">
-                                      <a href="{{ route('detailBerita', $articles['data'][1]['slug']) }}"><img
-                                              src="{{ $articles['data'][1]['thumbnail'] }}" alt=""
-                                              style="width: 100px; height:
-                                      100px; object-fit: cover;"></a>
-                                      <div class="w-100 d-flex flex-column justify-content-center bg-light px-3"
-                                          style="height: 100px;">
-                                          <div class="mb-1" style="font-size: 13px;">
-                                              <a href="">{{ $articles['data'][1]['category'] }}</a>
-                                              <span class="px-1">/</span>
-                                              <span>{{ $articles['data'][1]['published_at'] }}</span>
-                                          </div>
-                                          <a class="h6 m-0"
-                                              href="">{{ Str::limit($articles['data'][1]['title'], 55, '....') }}</a>
-                                      </div>
-                                  </div>
-                                  <div class="d-flex mb-3">
-                                      <a href="{{ route('detailBerita', $articles['data'][2]['slug']) }}"><img
-                                              src="{{ $articles['data'][2]['thumbnail'] }}" alt=""
-                                              style="width: 100px; height:
-                                      100px; object-fit: cover;"></a>
-                                      <div class="w-100 d-flex flex-column justify-content-center bg-light px-3"
-                                          style="height: 100px;">
-                                          <div class="mb-1" style="font-size: 13px;">
-                                              <a href="">{{ $articles['data'][2]['category'] }}</a>
-                                              <span class="px-1">/</span>
-                                              <span>{{ $articles['data'][2]['published_at'] }}</span>
-                                          </div>
-                                          <a class="h6 m-0"
-                                              href="">{{ Str::limit($articles['data'][2]['title'], 55, '....') }}</a>
-                                      </div>
-                                  </div>
-                                  <div class="d-flex mb-3">
-                                      <a href="{{ route('detailBerita', $articles['data'][3]['slug']) }}"><img
-                                              src="{{ $articles['data'][3]['thumbnail'] }}" alt=""
-                                              style="width: 100px; height:
-                                      100px; object-fit: cover;"></a>
-                                      <div class="w-100 d-flex flex-column justify-content-center bg-light px-3"
-                                          style="height: 100px;">
-                                          <div class="mb-1" style="font-size: 13px;">
-                                              <a href="">{{ $articles['data'][3]['category'] }}</a>
-                                              <span class="px-1">/</span>
-                                              <span>{{ $articles['data'][3]['published_at'] }}</span>
-                                          </div>
-                                          <a class="h6 m-0"
-                                              href="">{{ Str::limit($articles['data'][3]['title'], 55, '....') }}</a>
-                                      </div>
-                                  </div>
-                                  <div class="d-flex mb-3">
-                                      <a href="{{ route('detailBerita', $articles['data'][4]['slug']) }}"><img
-                                              src="{{ $articles['data'][4]['thumbnail'] }}" alt=""
-                                              style="width: 100px; height:
-                                      100px; object-fit: cover;"></a>
-                                      <div class="w-100 d-flex flex-column justify-content-center bg-light px-3"
-                                          style="height: 100px;">
-                                          <div class="mb-1" style="font-size: 13px;">
-                                              <a href="">{{ $articles['data'][4]['category'] }}</a>
-                                              <span class="px-1">/</span>
-                                              <span>{{ $articles['data'][4]['published_at'] }}</span>
-                                          </div>
-                                          <a class="h6 m-0"
-                                              href="">{{ Str::limit($articles['data'][4]['title'], 55, '....') }}</a>
-                                      </div>
-                                  </div>
+                                  @endforeach
                               </div>
                               <!-- Popular News End -->
 
@@ -138,12 +76,10 @@
                                       <h3 class="m-0">Tags</h3>
                                   </div>
                                   <div class="d-flex flex-wrap m-n1">
-                                      <a href="#"
-                                          class="btn btn-sm btn-outline-secondary m-1">{{ $articles['data'][0]['tag'][0] }}</a>
-                                      <a href="#" class="btn btn-sm btn-outline-secondary m-1">{{ $articles['data'][1]['tag'][0] }}</a>
-                                      <a href="#" class="btn btn-sm btn-outline-secondary m-1">{{ $articles['data'][2]['tag'][0] }}</a>
-                                      <a href="#" class="btn btn-sm btn-outline-secondary m-1">{{ $articles['data'][3]['tag'][0] }}</a>
-                                      <a href="#" class="btn btn-sm btn-outline-secondary m-1">{{ $articles['data'][4]['tag'][0] }}</a>
+                                      @foreach ($detailArticles['tag'] as $tag)
+                                          <a href="#"
+                                              class="btn btn-sm btn-outline-secondary m-1">{{ $tag }}</a>
+                                      @endforeach
                                   </div>
                               </div>
                           </div>
